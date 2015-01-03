076cb9f4c8143d5b8f330a56697ca76950c20d0a
Anthony Ettinger <anthony@chovy.com>
2015-01-03 02:41:46 -0800

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# getrandomdate
+
+Get a random date between two dates in JSON format.
+
+# /days-ago/:days
+
+	GET /days-ago/7
+
+Gets a random date from the last 7 days.
+
+# /hours-ago/:hours
+
+	GET /hours-ago/4
+
+Gets a random date from the last 4 hours.
+
+# /seconds-ago/:90
+
+	GET /seconds-ago/90
+
+Gets a random date from the last 90 seconds.
+
---
+++ server/index.js
@@ -0,0 +1,48 @@
+var moment = require('moment');
+var express = require('express')
+var app = express()
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getDataFromDate(rand){
+ 	return {
+		unix: moment(rand).utc().unix(),
+		format: moment(rand).utc().format(),
+		fromNow: moment(rand).utc().fromNow(),
+		utc: moment(rand).utc(),
+		utcMs: moment(rand).utc().valueOf(),
+		full: moment(rand).utc().format('MMMM Do YYYY, h:mm:ss a'),
+		dayOfWeek: moment(rand).utc().format('dddd'),
+		calendar: moment(rand).utc().calendar()
+	};
+}
+
+app.get('/', function(req, res){
+	//readme.md
+});
+
+app.get('/days-ago/:days', function (req, res) {
+	var days = parseInt(req.params.days);
+	//returns seconds
+	var start = moment().unix();
+	//needs ms as input
+	var end = moment(start*1000).subtract(days, 'd').unix();
+	//convert to ms for moment
+	var rand = start*1000;//getRandomInt(start, end)*1000;
+	console.log(start, end, rand)
+
+	var data = getDataFromDate(rand);
+	
+  res.send(data);
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
